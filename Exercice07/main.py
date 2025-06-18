146630ea7a3557918a689adc996828188ef8cc82
Faaab84 <fabien.balaramane@outlook.fr>
2025-06-18 18:05:08 +0200

    envoi

---
+++ Exercice07/main.py
@@ -0,0 +1,12 @@
+def square(nombre):
+    try:
+        result = float(nombre)
+        return result ** 2
+    except (ValueError, TypeError):
+        print("Le paramètre doit être un nombre !")
+
+
+nombre = input("Entrez un nombre : ")
+square_resultat = square(nombre)
+if square_resultat is not None:
+    print(f"Le carré est : {square_resultat}")
