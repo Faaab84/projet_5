48bbb4d3609e73bb0645f532e0e571a3a7e45c25
Faaab84 <fabien.balaramane@outlook.fr>
2025-06-22 10:59:05 +0200

    ajout exemple

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,11 +1,9 @@
-# Fonction calculate_average
 def calculate_average(numbers):
     total = sum(numbers)
     length = len(numbers)
     return total / length
 
 
-# Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
 
 average = calculate_average(numbers)
